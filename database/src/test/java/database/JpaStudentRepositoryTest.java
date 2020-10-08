52dfe6932cd82c58bfdc12d7f593c97b3578d3d1
Hanh Nguyen <hanh845@gmail.com>
2020-10-09 06:11:36 +0700

    add test repository

---
+++ database/src/test/java/database/JpaStudentRepositoryTest.java
@@ -0,0 +1,38 @@
+package database;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sample.student.DemoApplication;
+import com.sample.student.Student;
+import com.sample.student.StudentRepository;
+
+@RunWith(SpringRunner.class)
+@TestPropertySource("classpath:application.properties")
+@SpringBootTest(classes = DemoApplication.class)
+public class JpaStudentRepositoryTest {
+	
+	@Autowired
+	@Qualifier("jpaStudentRepositoryImpl")
+	StudentRepository studentRepository;
+
+	@Test
+	void testSaveStudent() {
+		
+		Student student = new Student(6l, "marry", "1");
+		
+		int id = studentRepository.saveStudent(student);
+		
+		Student expected = studentRepository.findById(6l);
+		
+		assertEquals(id, expected.getId());
+	}
+
+}
